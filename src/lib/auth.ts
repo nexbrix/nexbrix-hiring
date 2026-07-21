8d335f5e2523fccdf2ca804d5a59e6be77344074
JeetDas5 <jeet15083011@gmail.com>
2026-07-21 12:13:51 +0530

    feat: write schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,10 @@
+import { prisma } from "./prisma";
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-// If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/generated/prisma/client";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
-    provider: "sqlite", // or "mysql", "postgresql", ...etc
+    provider: "postgresql",
   }),
   emailAndPassword: {
     enabled: true,
